68582ff28dafbaa3848924c2fe888cbcd388223b
Anthony DeDominic <adedomin@gmail.com>
2022-06-05 15:43:47 -0400

    nojs search

--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,5 @@
 use crate::html;
-use crate::moosedb::{Moose, MooseDb};
+use crate::moosedb::{Moose, MooseDb, MoosePage};
 use crate::render::{moose_png, IrcArt};
 use lazy_static::lazy_static;
 use rand::Rng;
@@ -8,6 +8,7 @@ use rouille::{
     percent_encoding::{percent_encode, NON_ALPHANUMERIC},
     router, Request, Response,
 };
+use std::borrow::Cow;
 use std::sync::{Arc, RwLock, RwLockReadGuard};
 
 const RANDOM: &str = "random";
@@ -16,6 +17,35 @@ const LATEST: &str = "latest";
 fn get_query_params(q: &str) -> impl Iterator<Item = (&str, &str)> {
     q.split_terminator('&')
         .map(|kv| kv.split_once('=').unwrap_or((kv, "")))
+}
+
+fn get_query_param_value<'m>(qstring: &'m str, param: &str) -> Cow<'m, str> {
+    let ret = get_query_params(qstring)
+        .find(|(key, _)| *key == param)
+        .map(|(_, val)| val)
+        .unwrap_or("");
+    percent_decode(ret.as_bytes()).decode_utf8_lossy()
+}
+
+fn error_resp(code: u16, res_type: &'static str, message: String) -> Response {
+    let mut e = Response::from_data(res_type, message);
+    e.status_code = code;
+    e
+}
+
+fn json_err_res(code: u16, message: &str) -> Response {
+    error_resp(
+        code,
+        "application/json",
+        format!(
+            r#"{{"status":"error","msg":{}}}"#,
+            serde_json::to_string::<str>(message).unwrap(),
+        ),
+    )
+}
+
+fn moose_404(moose_name: &str) -> Response {
+    json_err_res(404, &format!("no such moose: {}", moose_name))
 }
 
 fn simple_get<'m>(
@@ -74,36 +104,36 @@ pub fn handler(db: Arc<RwLock<MooseDb>>, req: &Request) -> Response {
                 let rand_idx = rand::thread_rng().gen_range(0..max_page);
                 return Response::redirect_303(format!("/gallery/{}", rand_idx));
             }
+            "/gallery/nojs-search" => {
+                let query = get_query_param_value(req.raw_query_string(), "q");
+                let unlocked = db.read().unwrap();
+                let meese = unlocked
+                    .find_page(&query)
+                    .iter()
+                    .cloned()
+                    .cloned()
+                    .collect::<Vec<Moose>>();
+                let html = html::gallery_page(MoosePage(&meese[..]), 0, 0);
+                let html_crc = crc32fast::hash(html.as_bytes()).to_string();
+                return Response::from_data("text/html", html).with_etag(req, html_crc);
+            }
             "/page" => {
                 let page_count = { db.read().unwrap().page_count() };
                 return Response::from_data("application/json", format!("{}", page_count));
             }
             "/search" => {
-                let query = get_query_params(req.raw_query_string())
-                    .find(|(key, _)| *key == "q")
-                    .map(|(_, val)| val)
-                    .unwrap_or("");
-                let query = percent_decode(query.as_bytes()).decode_utf8_lossy();
+                let query = get_query_param_value(req.raw_query_string(), "q");
 
                 if query.len() > 50 {
-                    let mut e = Response::from_data(
-                        "application/json",
-                        r#"{"status":"error","msg":"query length too long"}"#.to_owned(),
-                    );
-                    e.status_code = 400u16;
-                    return e;
+                    return json_err_res(400, "query is too large (max 50)");
                 } else if query.is_empty() {
-                    let mut e = Response::from_data(
-                        "application/json",
-                        r#"{"status":"error","msg":"query is empty"}"#.to_owned(),
-                    );
-                    e.status_code = 400u16;
-                    return e;
+                    return json_err_res(400, "query is empty");
                 } else {
                     let unlocked = db.read().unwrap();
                     let meese = unlocked.find_page_with_link_bin(&query);
                     let meese_crc = crc32fast::hash(&meese).to_string();
-                    return Response::from_data("application/json", meese).with_etag(req, meese_crc);
+                    return Response::from_data("application/json", meese)
+                        .with_etag(req, meese_crc);
                 }
             }
             _ => (),
@@ -116,9 +146,7 @@ pub fn handler(db: Arc<RwLock<MooseDb>>, req: &Request) -> Response {
             match simple_get(&db_locked, &moose_name) {
                 Ok(Some(moose)) => Response::from_data("application/json", moose).with_public_cache(3600),
                 Ok(None) => {
-                    let mut e = Response::text(format!("no such moose: {}", moose_name));
-                    e.status_code = 404u16;
-                    e
+                    moose_404(&moose_name)
                 },
                 Err(redir) => Response::redirect_303(format!("/moose/{}", redir)),
             }
@@ -128,9 +156,7 @@ pub fn handler(db: Arc<RwLock<MooseDb>>, req: &Request) -> Response {
             match simple_get(&db_locked, &moose_name) {
                 Ok(Some(moose)) => Response::from_data("text/irc-art", IrcArt::from(moose)).with_public_cache(3600),
                 Ok(None) => {
-                    let mut e = Response::text(format!("no such moose: {}", moose_name));
-                    e.status_code = 404u16;
-                    e
+                    moose_404(&moose_name)
                 },
                 Err(redir) => Response::redirect_303(format!("/irc/{}", redir)),
             }
@@ -143,16 +169,12 @@ pub fn handler(db: Arc<RwLock<MooseDb>>, req: &Request) -> Response {
                         Ok(png) => Response::from_data("image/png", png).with_public_cache(3600),
                         Err(e) => {
                             // bad error...
-                            let mut e = Response::text(e.to_string());
-                            e.status_code = 500;
-                            e
+                            json_err_res(500, &e.to_string())
                         }
                     }
                 },
                 Ok(None) => {
-                    let mut e = Response::text(format!("no such moose: {}", moose_name));
-                    e.status_code = 404u16;
-                    e
+                    moose_404(&moose_name)
                 },
                 Err(redir) => Response::redirect_303(format!("/img/{}", redir)),
             }
@@ -164,7 +186,6 @@ pub fn handler(db: Arc<RwLock<MooseDb>>, req: &Request) -> Response {
             let html_crc = crc32fast::hash(html.as_bytes()).to_string();
             Response::from_data("text/html", html).with_etag(req, html_crc)
         },
-
         (GET) (/page/{pid: usize}) => {
             let db_locked = db.read().unwrap();
             let meese = db_locked.get_page(pid);
