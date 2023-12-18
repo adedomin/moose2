6b5addee4a783195c4e1601e1d0016c9effa47d3
Anthony DeDominic <adedomin@gmail.com>
2023-12-18 14:30:02 -0500

    remove some code out of the select macro

--- server/src/task/dump_moose.rs
+++ server/src/task/dump_moose.rs
@@ -6,7 +6,7 @@ use std::{
 
 use tokio::{sync::broadcast::Receiver, task::JoinHandle, time};
 
-use crate::db::{query::DUMP_MOOSE, Pool};
+use crate::db::{query::DUMP_MOOSE, Connection, Pool};
 use crate::model::{self};
 
 #[derive(thiserror::Error, Debug)]
@@ -20,6 +20,40 @@ pub enum DumpTaskError {
     #[error("Deserialization Error: {0}")]
     Serde(#[from] serde_json::Error),
 }
+
+async fn dump_moose_real(con: Connection, moose_dump: PathBuf) -> Result<(), DumpTaskError> {
+    con.interact(move |con| -> Result<(), DumpTaskError> {
+        let file = std::fs::File::create(moose_dump)?;
+        let mut bufw = BufWriter::new(file);
+        let mut start = true;
+
+        let mut q = con.prepare_cached(DUMP_MOOSE)?;
+        let mut w = q.query([])?;
+        while let Ok(Some(row)) = w.next() {
+            if start {
+                bufw.write(b"[")?;
+                start = false;
+            } else {
+                bufw.write(b",")?;
+            }
+            let moose = model::moose::Moose {
+                name: row.get(0)?,
+                image: row.get(1)?,
+                dimensions: row.get(2)?,
+                created: row.get(3)?,
+                author: row.get(4)?,
+                upvotes: row.get(5)?,
+            };
+            let moose = serde_json::to_vec(&moose)?;
+            bufw.write(&moose)?;
+        }
+        bufw.write(b"]")?;
+        println!("INTO: [DUMP] Done dumping moose.");
+        Ok(())
+    })
+    .await?
+}
+
 async fn dump_moose(
     moose_dump: PathBuf,
     db: Pool,
@@ -33,39 +67,10 @@ async fn dump_moose(
                 return Ok(());
             },
             _ = interval.tick() => {
-                println!("INFO: [DUMP] Timer Triggered, dumping Moose to json file.");
+                println!("INFO: [DUMP] Timer Triggered, dumping Moose to json file: {moose_dump:?}");
                 let con = db.get().await.unwrap();
                 let md = moose_dump.clone();
-                con.interact(move |con| -> Result<(), DumpTaskError> {
-                    let file = std::fs::File::create(md)?;
-                    let mut bufw = BufWriter::new(file);
-                    let mut start = true;
-
-                    let mut q = con.prepare_cached(DUMP_MOOSE)?;
-                    let mut w = q.query([])?;
-                    while let Ok(Some(row)) = w.next() {
-                        if start {
-                            bufw.write(b"[")?;
-                            start = false;
-                        } else {
-                            bufw.write(b",")?;
-                        }
-                        let moose = model::moose::Moose {
-                            name: row.get(0)?,
-                            image: row.get(1)?,
-                            dimensions: row.get(2)?,
-                            created: row.get(3)?,
-                            author: row.get(4)?,
-                            upvotes: row.get(5)?,
-                        };
-                        let moose = serde_json::to_vec(&moose)?;
-                        bufw.write(&moose)?;
-                    }
-                    bufw.write(b"]")?;
-                    println!("INTO: [DUMP] Done dumping moose.");
-                    Ok(())
-                })
-                .await??;
+                dump_moose_real(con, md).await?;
             }
         }
     }
@@ -76,10 +81,7 @@ pub fn dump_moose_task(
     db: Pool,
     stop_broadcast: Receiver<bool>,
 ) -> JoinHandle<Result<(), DumpTaskError>> {
-    println!(
-        "INFO: [DUMP] Setting up Auto-dumps of database to: {:?}",
-        moose_dump
-    );
+    println!("INFO: [DUMP] Setting up Auto-dumps of database.");
     tokio::spawn(async move {
         let e = dump_moose(moose_dump, db, stop_broadcast).await;
         println!("WARN: [DUMP] Task has shut down: {:?}", e);
