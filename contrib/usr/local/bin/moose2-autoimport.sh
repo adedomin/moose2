c187738ff7dc07956617900e72463460b7079358
Anthony DeDominic <adedomin@gmail.com>
2024-09-17 11:25:19 -0400

    fixup systemd services and auto importer

--- contrib/usr/local/bin/moose2-autoimport.sh
+++ contrib/usr/local/bin/moose2-autoimport.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-/usr/local/bin/moose2 import \
-    -f ~moose/.local/var/dump.json \
-    -o /var/lib/moose2/moose.json || exit
-systemctl reload moose2
+#!/bin/bash
+trap 'rm -f -- "$tmp"' EXIT
+tmp="$(mktemp)" || exit
+# $TRIGGER_PATH is set when this service is invoked by a path unit trigger.
+/usr/local/bin/moose2 --config "${MOOSE2_CONFIG?}" \
+    convert \
+        "${TRIGGER_PATH-/home/moose/.local/var/dump.json}" \
+        "$tmp" || exit
+/usr/local/bin/moose2 --config "${MOOSE2_CONFIG?}" \
+    import --merge "$tmp" || exit
+# TODO: not necessary, for now.
+# systemctl reload moose2
