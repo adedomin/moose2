4a7996919f1b0c4f8b99d2d2672338c4ea6389a1
Anthony DeDominic <adedomin@gmail.com>
2025-03-15 08:20:44 -0400

    tighten state/runtime directory permission

--- contrib/usr/local/bin/moose2-autoimport.sh
+++ contrib/usr/local/bin/moose2-autoimport.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
-trap 'rm -f -- "$tmp"' EXIT
-tmp="$(mktemp)" || exit
+#!/bin/sh
 # $TRIGGER_PATH is set when this service is invoked by a path unit trigger.
-/usr/local/bin/moose2 --config "${MOOSE2_CONFIG?}" \
-    convert \
-        "${TRIGGER_PATH-/home/moose/.local/var/dump.json}" \
-        "$tmp" || exit
-/usr/local/bin/moose2 --config "${MOOSE2_CONFIG?}" \
-    import --merge "$tmp" || exit
-# TODO: not necessary, for now.
-# systemctl reload moose2
+/usr/local/bin/moose2 \
+    --config "${MOOSE2_CONFIG?}" \
+    import --ignore "${TRIGGER_PATH-/home/moose/.local/var/dump.json}"
